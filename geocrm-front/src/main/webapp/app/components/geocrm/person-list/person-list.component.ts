dba18fe8e4d10f8065fba739af681c429f4dc9d4
adrohms <adrom7.0@gmail.com>
2023-05-03 11:50:44 -0300

    Adjusting ulgy styles and starting the implementation of pagination and person form

--- geocrm-front/src/main/webapp/app/components/geocrm/person-list/person-list.component.ts
+++ geocrm-front/src/main/webapp/app/components/geocrm/person-list/person-list.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { PageEvent } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
-import { IPage } from 'app/shared/types/page.interface';
-import { Observable } from 'rxjs';
 
 import { IPerson } from '../models/Person.model';
 import { PersonService } from '../services/person.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'max-person-list',
@@ -13,20 +13,58 @@ import { PersonService } from '../services/person.service';
 })
 export class PersonListComponent implements OnInit {
 
-  public displayedColumns: string[] = ['id', 'name', 'taxId', 'code', 'email', 'phone'];
-  public dataSource: any;
+  public displayedColumns: string[] = ['id', 'name', 'taxId', 'code', 'email', 'phone', 'options'];
 
-  constructor(private personService: PersonService) {}
+  public dataSource: any;
+  public length = 50;
+  public pageSize = 10;
+  public pageIndex = 0;
+  public pageSizeOptions = [5, 10, 25];
+
+  public hidePageSize = false;
+  public showPageSizeOptions = true;
+  public showFirstLastButtons = true;
+  public disabled = false;
+
+  public pageEvent?: PageEvent;
+
+  constructor(
+    private personService: PersonService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
-
     this.personService.persons$.subscribe(value => {
       if(value?.content != null && value.content.length > 0) {
         const personList: IPerson[] = value.content;
         this.dataSource = new MatTableDataSource(personList);
       }
     })
+  }
 
+  handlePageEvent(pageEvent: PageEvent): void {
+    this.pageEvent = pageEvent;
+    this.length = pageEvent.length;
+    this.pageSize = pageEvent.pageSize;
+    this.pageIndex = pageEvent.pageIndex;
+  }
+
+  setPageSizeOptions(setPageSizeOptionsInput: string): void {
+    if (setPageSizeOptionsInput) {
+      this.pageSizeOptions = setPageSizeOptionsInput.split(',').map(str => +str);
+    }
+  }
+
+  createPerson(): void {
+    this.router.navigate(['/geo-crm/form/']);
+  }
+
+  editPerson(person: IPerson): void {
+    this.router.navigate([`/geo-crm/form/${person.id!.toString()}`])
+  }
+
+  deletePerson(person: IPerson): void {
+    this.personService
   }
 
 }
--- geocrm-front/src/main/webapp/app/shared/shared.module.ts
+++ geocrm-front/src/main/webapp/app/shared/shared.module.ts
@@ -1,4 +1,12 @@
 import { NgModule } from '@angular/core';
+import { MatLineModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatTableModule } from '@angular/material/table';
 
 import { AlertErrorComponent } from './alert/alert-error.component';
 import { AlertComponent } from './alert/alert.component';
@@ -11,12 +19,6 @@ import { ItemCountComponent } from './pagination/item-count.component';
 import { SharedLibsModule } from './shared-libs.module';
 import { SortByDirective } from './sort/sort-by.directive';
 import { SortDirective } from './sort/sort.directive';
-import { MatExpansionModule } from '@angular/material/expansion';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatIconModule } from '@angular/material/icon';
-import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatTableModule } from '@angular/material/table';
-import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   imports: [
@@ -26,7 +28,11 @@ import { MatInputModule } from '@angular/material/input';
     MatIconModule,
     MatDatepickerModule,
     MatTableModule,
-    MatInputModule
+    MatInputModule,
+    MatPaginatorModule,
+    MatInputModule,
+    MatLineModule,
+    MatDatepickerModule
   ],
   declarations: [
     AlertComponent,
@@ -57,7 +63,11 @@ import { MatInputModule } from '@angular/material/input';
     MatIconModule,
     MatDatepickerModule,
     MatTableModule,
-    MatInputModule
+    MatInputModule,
+    MatPaginatorModule,
+    MatInputModule,
+    MatLineModule,
+    MatDatepickerModule
   ],
 })
 export class SharedModule {}
